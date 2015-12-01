fa0fc5ccc0c927fc3b4f0ae9c0d1f88f9ad89c7e
ChillMeister <some.dude@the.internet>
2015-11-30 22:54:06 -0600

    initial commit

---
+++ addpm.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         AddPmButtonToUsernameAndDoStuff
+// @namespace    http://tampermonkey.net/
+// @version      0.36
+// @description  does stuff
+// @author       Chanterelle
+// @match        https://broadcasthe.net/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+/*jQuery = $ because old jQuery*/
+ 
+(function($) {
+    $(document).ready(function(){
+        // Search
+        $('#user .box.pad.center tbody tr > td a').each(function(){
+            var id = $(this).attr('href').replace('user.php?id=','');
+            $(this).after('<a href="inbox.php?action=compose&to='+ id + '">[PM]</a> ');
+        });
+
+        // Forums
+        $('#forums .thin table tr.colhead_dark').each(function(){
+            var id = $("td strong a", this).attr('href').replace('user.php?id=','');
+            $("td > span + span", this).prepend('<a href="inbox.php?action=compose&to='+ id + '">[PM]</a>&nbsp;');
+        });
+
+        // Torrent detail page
+        $('tr.pad > td > blockquote:nth-child(6) > a').each(function() {
+            var id = $(this).attr('href').replace('user.php?id=','');
+            $(this).after('&nbsp;<a href="inbox.php?action=compose&to='+ id + '">[PM]</a> ');
+        });
+
+        // Torrents page
+        $('#torrent_table > tbody > tr.torrent').each(function() {
+            var link = $('td:nth-child(3) > div.nobr > a', this);
+            // Some torrents are uploaded by AutoUp or are anonymous and have no link
+            if(link && link.attr('href')) {
+                var id = link.attr('href').replace('user.php?id=', '');
+                link.after('&nbsp;<a href="inbox.php?action=compose&to='+ id + '">[PM]</a> ');
+            }
+        });
+
+        // Hide obnoxious file list
+        (function() {
+            $('tr.pad > td > div.linkbox').each(function() {
+                var $this = $(this);
+                var filelist = $this.next().next();
+                $('<a>',{
+                    text: '[File List]',
+                    href: '#',
+                    click: function(){
+                        filelist.toggle();
+                        return false;
+                    }
+                }).prependTo($this);
+                filelist.hide();
+                
+                var filenum = filelist.find('tbody').children().length - 1;
+                var output = ' (' + filenum + ' file';
+                if(filenum > 1) output += 's';
+                output += ')';
+                $this.parent().parent().prev().prev().children()[1].innerHTML += output;
+            });
+        }());
+    });
+}(jQuery));
